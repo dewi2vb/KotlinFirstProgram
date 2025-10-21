6e5b5ddf270413422d2fd029717a06afdd2631ea
dewi2vb <dewi2vb@gmail.com>
2025-10-21 23:14:55 +0700

    First Commit

--- src/Boolean.kt
+++ src/Boolean.kt
@@ -2,7 +2,7 @@ fun main(args: Array<String>) {
     var varBool : Boolean = true
     if(varBool)
         println("varBool bernilai " + varBool)
-    var varInt : Int = 1
-    if(varInt) //kode ini menyebabkan eror karana diisi tipe data int harusnya boolean
-        println("varInt bernilai " + varInt)
+ //   var varInt : Int = 1
+   // if(varInt) //kode ini menyebabkan eror karana diisi tipe data int harusnya boolean
+     //   println("varInt bernilai " + varInt)
 }
---
+++ src/String1.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val myName: String = "Isna Yulia"
+    for(chr in myName){
+        print(chr)
+    }
+    print('\n')
+}
